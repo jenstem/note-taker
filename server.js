ccb2fb83eb9a1a1f739edc9e18308a285015df65
jenstem <124745888+jenstem@users.noreply.github.com>
2023-08-12 17:08:11 -0400

    Add get index and get notes to server.js

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const path = require('path');
+const { readFromFile, writeToFile } = require('./helpers');
 
 const app = express();
-// const PORT = 3001;
-app.request(express.static('public'));
+const PORT = 3001;
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // get index
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../..public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // get notes
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-// routes - Do I need this???
-// app.get('/routes', (req, res) =>
-//     res.sendFile(path.join(__dirname, 'public/routes.html'))
-// );
+app.get("/api/notes", async (req, res) => {
+    let notes = await readFromFile('db/db.json');
+    let parsedNotes = JSON.parse(notes);
+    res.json(parsedNotes);
+})
 
-// app.listen(PORT, () =>
-//     console.log('Serving static asset routes at port http://localhost:${PORT}')
-// );
+app.post("/api/notes", async (req, res) => {
+    let notes = await readFromFile('db/db.json');
+    let parsedNotes = JSON.parse(notes);
+    let { title, text } = req.body;
+    let newNote = { title, text };
+    let updatedNotes = [...parsedNotes, newNote];
+    await writeToFile('db/db.json', JSON.stringify(updatedNotes));
+    let overwrittenNotes = await readFromFile('db/db.json');
+    res.json(overwrittenNotes);
+})
+
+app.listen(PORT, () =>
+    console.log('Serving static asset routes at port http://localhost:${PORT}')
+);
 
 module.exports = express;
