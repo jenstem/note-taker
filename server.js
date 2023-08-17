b6b1c08aa41e6cfb0bd1964b85ddae4507dcf8e1
jenstem <124745888+jenstem@users.noreply.github.com>
2023-08-17 10:38:53 -0400

    Revise const PORT on server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const { readFromFile, writeToFile } = require('./helpers');
 const uuid = require('uuid');
 
 const app = express();
-const PORT = 3001;
+// const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
